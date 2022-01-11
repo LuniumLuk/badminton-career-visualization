3039d49980900a452c1bd85a420f14082a54eebc
Michael.Luk <52308484+LuniumLuk@users.noreply.github.com>
2022-01-11 19:55:07 +0800

    upload build

---
+++ dist/static/js/data.js
@@ -0,0 +1,62 @@
+module.exports = FIGURE_DATA = {
+    'momota': {
+        'info': [
+          { 'item': 'Country', 'value': 'Japan' },
+          { 'item': 'Born', 'value': 'September 1994 (age 26) Mino, Kagawa, Japan' },
+          { 'item': 'Height', 'value': '1.75 m (5 ft 9 in)' },
+          { 'item': 'Handedness', 'value': 'Left' }
+        ],
+        'figureImg': '../../static/images/momota-figure.png',
+        'backImg': '../../static/images/momota-back.png',
+        'intro': 'Kento Momota is a Japanese badminton player. He is known to have a skillful and relentless play style on court. He has won several major badminton tournaments including two World Championships titles, two Asian Championships titles, and one All England title. Recently, Kento Momota entered into Guinness Book of World Records for “The most Badminton Men’s Singles Titles in a Season – 11", between March 3 and December 15, 2019.'
+    },
+
+    'shiyuqi': {
+        'info': [
+          { 'item': 'Country', 'value': 'China' },
+          { 'item': 'Born', 'value': '28 February 1996 (age 24) Nantong, Jiangsu, China' },
+          { 'item': 'Height', 'value': '1.84 m (6 ft 0 in)' },
+          { 'item': 'Handedness', 'value': 'Right' }
+        ],
+        'figureImg': '../../static/images/shiyuqi-figure.png',
+        'backImg': '../../static/images/shiyuqi-back.png',
+        'intro': 'Shi Yuqi is a Chinese badminton player. Shi Yuqi won his first Superseries title in the 2016 French Open. At the 2017 All England Open, he defeated 6-time champion Lin Dan to reach the final, and repeated the same feat again at the 2018 All England Open, where he outclassed Lin Dan in the tournament final, winning the most prestigious tournament of his career.'
+    },
+
+    'axelsen': {
+        'info': [
+          { 'item': 'Country', 'value': 'Denmark' },
+          { 'item': 'Born', 'value': '4 January 1994 (age 27) Odense, Denmark' },
+          { 'item': 'Height', 'value': '1.94 m (6 ft 4 in)' },
+          { 'item': 'Handedness', 'value': 'Right' }
+        ],
+        'figureImg': '../../static/images/axelsen-figure.png',
+        'backImg': '../../static/images/axelsen-back.png',
+        'intro': 'Viktor Axelsen (born 4 January 1994) is a Danish badminton player. He was the 2017 World Champion and the bronze medalist at the 2016 Summer Olympics. He won the 2010 World Junior Championships, beating Korea\'s Kang Ji-wook in the final to become the first ever European player to hold the title. Axelsen was crowned European men\'s singles champion in 2016 and 2018.'
+    },
+
+    'lindan': {
+        'info': [
+          { 'item': 'Country', 'value': 'China' },
+          { 'item': 'Born', 'value': '14 October 1983 (age 37) Longyan, Fujian, China' },
+          { 'item': 'Height', 'value': '178 cm (5 ft 10 in)' },
+          { 'item': 'Handedness', 'value': 'Left' }
+        ],
+        'figureImg': '../../static/images/lindan-figure.png',
+        'backImg': '../../static/images/lindan-back.png',
+        'intro': 'Lin Dan is a former Chinese professional badminton player. He is a two-time Olympic champion, five-time World champion, as well as a six-time All England champion. Widely regarded as the greatest badminton player of all time, by the age of 28 Lin had completed the "Super Grand Slam", having won what some consider to be the nine major titles in the badminton world: Olympic Games, World Championships, World Cup, Thomas Cup, Sudirman Cup, Super Series Masters Finals, All England Open, Asian Games, and Asian Championships, becoming the first and only player to achieve this feat.'
+    },
+
+    'leechongwei': {
+        'info': [
+          { 'item': 'Country', 'value': 'Malaysia' },
+          { 'item': 'Born', 'value': '21 October 1982 (age 38) Bagan Serai, Perak, Malaysia' },
+          { 'item': 'Height', 'value': '1.72 m (5 ft 7 1⁄2 in)' },
+          { 'item': 'Handedness', 'value': 'Right' }
+        ],
+        'figureImg': '../../static/images/leechongwei-figure.png',
+        'backImg': '../../static/images/leechongwei-back.png',
+        'intro': 'Datuk Lee Chong Wei DB PJN AMN DCSM DSPN (born 21 October 1982) is a former Malaysian badminton player. As a singles player, Lee was ranked first worldwide for 349 weeks, including a 199-week streak from 21 August 2008 to 14 June 2012. He is the fourth Malaysian player after Rashid Sidek, Roslin Hashim and Wong Choong Hann to achieve such a ranking (since official rankings were first kept in the 1980s), and is the only Malaysian shuttler who has held the number one ranking for more than a year.'
+    },
+}
+
